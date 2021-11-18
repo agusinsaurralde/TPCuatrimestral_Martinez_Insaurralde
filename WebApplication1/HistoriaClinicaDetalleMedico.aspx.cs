23368ad86731c83dac0b52d39ead8d52dda7a1a9
agusinsaurralde <agusinsaurralde28@gmail.com>
2021-11-18 01:35:23 -0300

    Ventanas Médico

---
+++ WebApplication1/HistoriaClinicaDetalleMedico.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Formulario_web16 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Click_Editar(object sender, EventArgs e)
+        {
+            Response.Redirect("EditarHistoriaClinicaMedico.aspx", false);
+        }
+    }
+}
