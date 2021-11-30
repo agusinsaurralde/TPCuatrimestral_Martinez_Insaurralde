55c99d61add4632cf29593f6011546c06ee75dbd
agusinsaurralde <agusinsaurralde28@gmail.com>
2021-11-30 16:00:34 -0300

    Parte de AsignarTurno

--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -10,6 +10,7 @@ namespace Dominio
     {
 
         public string DNI { get; set; }
+        public string NombreCompleto { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Email { get; set; }
--- WebApplication1/AsignarTurno.aspx.cs
+++ WebApplication1/AsignarTurno.aspx.cs
@@ -4,12 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using DBClinica;
 
 namespace WebApplication1
 {
     public partial class Formulario_web11 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            EspecialidadDB espDB = new EspecialidadDB();
+            MedicoDB medicoDB = new MedicoDB();
+            ConexionDB conexion = new ConexionDB();
+
+            if (!IsPostBack)
+            {
+                List<Especialidad> esp = espDB.lista();
+                ddlistEspecialidad.DataSource = esp;
+                ddlistEspecialidad.DataTextField = "Nombre";
+                ddlistEspecialidad.DataValueField = "ID";
+                ddlistEspecialidad.DataBind();
+                ddlistEspecialidad.Items.Insert(0, new ListItem("-- Seleccionar --", "0"));
+
+
+                List<Medico> medico = medicoDB.listarMedico();
+                Session["listaMedico"] = medico;
+               
+                ddlistMedico.DataSource = medico;
+                ddlistMedico.DataTextField = "NombreCompleto";
+                ddlistMedico.DataValueField = "ID";
+                ddlistMedico.DataBind();
+                ddlistMedico.Items.Insert(0, new ListItem("Seleccionar", "0"));
+
+            }
+
+        }
+        protected void ddlistEspecialidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ID = int.Parse(ddlistEspecialidad.SelectedItem.Value);
+            ddlistMedico.DataSource = ((List<Medico>)Session["listaMedico"]).FindAll(x => x.Especialidad.Id == ID);
+            ddlistMedico.DataBind();
+            ddlistMedico.Enabled = true;
+        }
+
+        protected void Click_Buscar(object sender, EventArgs e)
         {
 
         }
